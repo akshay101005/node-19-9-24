a44c01d4e0bf36a48847938c6ca1fd71810a538b
akshay101005 <akshaykumarsharma101005@gmail.com>
2024-09-19 12:32:54 +0530

    updation....

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+let connection = require('./connection');
+const student = require('./routes/student');
+const app = express();
+connection();
+app.use(student);
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("server is running on 3000")
+    }
+})
+//locahost:3000/add/student
